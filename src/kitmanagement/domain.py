cff15508a2385fa8d5295c1ba65bf0570778573e
Leonardo <antunesleo4@gmail.com>
2020-09-05 12:25:40 -0300

    Implemented Product class

--- src/kitmanagement/domain.py
+++ src/kitmanagement/domain.py
@@ -6,8 +6,32 @@ from src.base.domain import AggregateRoot
 
 class Product(AggregateRoot):
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str, SKU: str, cost: float, price: float, inventory_quantity: int):
+        self.__name = name
+        self.__SKU = SKU
+        self.__cost = cost
+        self.__price = price
+        self.__inventory_quantity = inventory_quantity
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def SKU(self) -> str:
+        return self.__SKU
+
+    @property
+    def cost(self) -> float:
+        return self.__cost
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @property
+    def inventory_quantity(self) -> int:
+        return self.__inventory_quantity
 
 
 class ProductRepository(ABC):
--- tests/unit/kitmanagement/test_domain.py
+++ tests/unit/kitmanagement/test_domain.py
@@ -1,11 +1,21 @@
 from unittest import mock
+
+from src.kitmanagement.domain import Product
 from tests.unit.base import TestCase
 
 
 class TestProduct(TestCase):
 
-    def test_should_assert_something(self):
-        self.assertTrue(True)
-
-    def test_should_assert_something_else(self):
-        self.assertEqual(1, 1)
+    def test_product_initialization_should_have_all_fields(self):
+        product = Product(
+            name='The Last of Us Part II',
+            SKU='AHJU-49685',
+            cost=10.00,
+            price=220.00,
+            inventory_quantity=150
+        )
+        self.assertEqual(product.name, 'The Last of Us Part II')
+        self.assertEqual(product.SKU, 'AHJU-49685')
+        self.assertEqual(product.cost, 10.00)
+        self.assertEqual(product.price, 220.00)
+        self.assertEqual(product.inventory_quantity, 150)
