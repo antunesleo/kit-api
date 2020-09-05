99504c71b267684abc4c1173d773f09e65907e59
Leonardo <antunesleo4@gmail.com>
2020-09-05 12:13:05 -0300

    Structuring tests folders an removing boilerplate tests left behind 2

---
+++ tests/integration/kitmanagement/test_repositories.py
@@ -0,0 +1,10 @@
+from tests.integration.base import TestCase
+
+
+class TestInMemoryProductRepository(TestCase):
+
+    def test_should_assert_something(self):
+        self.assertTrue(True)
+
+    def test_should_assert_something_else(self):
+        self.assertEqual(1, 1)
