611b4ebd5037033342e55045a621c11363098c00
Leonardo <antunesleo4@gmail.com>
2020-09-05 12:12:27 -0300

    Structuring tests folders an removing boilerplate tests left behind

--- tests/unit/kitmanagement/test_application_services.py
+++ tests/unit/kitmanagement/test_application_services.py
@@ -2,7 +2,7 @@ from unittest import mock
 from tests.unit.base import TestCase
 
 
-class TestClassA(TestCase):
+class TestProductsService(TestCase):
 
     def test_should_assert_something(self):
         self.assertTrue(True)
@@ -10,11 +10,3 @@ class TestClassA(TestCase):
     def test_should_assert_something_else(self):
         self.assertEqual(1, 1)
 
-
-class TestClassB(TestCase):
-
-    def test_should_assert_something(self):
-        self.assertTrue(True)
-
-    def test_should_assert_something_else(self):
-        self.assertEqual(1, 1)
--- tests/unit/kitmanagement/test_domain.py
+++ tests/unit/kitmanagement/test_domain.py
@@ -2,16 +2,7 @@ from unittest import mock
 from tests.unit.base import TestCase
 
 
-class TestClassA(TestCase):
-
-    def test_should_assert_something(self):
-        self.assertTrue(True)
-
-    def test_should_assert_something_else(self):
-        self.assertEqual(1, 1)
-
-
-class TestClassB(TestCase):
+class TestProduct(TestCase):
 
     def test_should_assert_something(self):
         self.assertTrue(True)
